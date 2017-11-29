53a0f8f70f42608186afeda875e78dc26eb99f57
Aditya Jagtiani <aditya.jagtiani@ajagtiani-mac.local>
2017-11-29 06:59:56 -0800

    Added unit test to confirm access key removal from yml files

--- src/test/java/com/appdynamics/extensions/aws/s3/S3MonitorITest.java
+++ src/test/java/com/appdynamics/extensions/aws/s3/S3MonitorITest.java
@@ -1,11 +1,20 @@
 package com.appdynamics.extensions.aws.s3;
 
+import static com.appdynamics.extensions.aws.util.AWSUtil.getAWSAccessKeysFromCfg;
+import static com.appdynamics.extensions.aws.util.AWSUtil.getAWSSecretKeysFromCfg;
+import static com.appdynamics.extensions.aws.util.AWSUtil.getConfigFilesFromDir;
 import static org.junit.Assert.assertTrue;
 
+import com.appdynamics.extensions.conf.MonitorConfiguration;
+import com.appdynamics.extensions.yml.YmlReader;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.singularity.ee.agent.systemagent.api.TaskOutput;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.List;
 import java.util.Map;
 
 public class S3MonitorITest {
@@ -28,5 +37,22 @@ public class S3MonitorITest {
 		
 		TaskOutput result = classUnderTest.execute(args, null);
 		assertTrue(result.getStatusMessage().contains("successfully completed"));
-	}	
+	}
+
+    @Test
+    public void testAccountKeysClearedFromConfig() {
+        File[] configFiles = getConfigFilesFromDir("src/main/resources/conf");
+        File[] configFilesForTests = getConfigFilesFromDir("src/test/resources/conf");
+
+        verifyBlankKeys(getAWSSecretKeysFromCfg(configFiles));
+        verifyBlankKeys(getAWSSecretKeysFromCfg(configFiles));
+        verifyBlankKeys(getAWSAccessKeysFromCfg(configFilesForTests));
+        verifyBlankKeys(getAWSSecretKeysFromCfg(configFilesForTests));
+    }
+
+    private void verifyBlankKeys(List<String> keys) {
+        for(String key : keys) {
+            assertTrue(key.equals(""));
+        }
+    }
 }
